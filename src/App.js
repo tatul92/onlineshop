cbba391971f030278d0ee390d548912849cab722
tatul92 <35904528+tatul92@users.noreply.github.com>
2023-02-04 21:13:14 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+        
+        <Header />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'> HOUSE </span>
+            <ul className='nav'>
+              <li>About</li>
+              <li>Contacts</li>
+              <li>Menu</li>
+
+
+            </ul>
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
